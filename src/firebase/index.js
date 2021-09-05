9d51442373134c9fb70067503d4315c02c63c0dc
Mallikarjuna M <42877673+Mallikarjunamatla@users.noreply.github.com>
2021-09-05 22:23:39 +0530

    removed sensitive information

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -5,14 +5,14 @@ import firebase from "firebase";
 
 
   const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyC8rYDg_2_SCusYXCUiwYqKatLFOX3wmnA",
+    apiKey: "YOUR-API-KEY",
     authDomain: "joke-meme-lines.firebaseapp.com",
     databaseURL: "https://joke-meme-lines-default-rtdb.asia-southeast1.firebasedatabase.app",
     projectId: "joke-meme-lines",
     storageBucket: "joke-meme-lines.appspot.com",
-    messagingSenderId: "784784414326",
-    appId: "1:784784414326:web:f8bde94f277ce3c6f1f157",
-    measurementId: "G-Q8TSR7BZY7"
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   });
 
   
